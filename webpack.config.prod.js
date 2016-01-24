290581d1eabb8dbc2812827bc9efe028784105e3
Ryan Ore <ryan@ryanore.com>
2016-01-24 16:47:26 -0600

    Build Config

---
+++ webpack.config.prod.js
@@ -0,0 +1,127 @@
+'use strict';
+//devserver
+var webpack = require('webpack');
+var path = require('path');
+var cssLoader = 'style-loader!css-loader!purifycss-loader';
+var scssLoader = 'style-loader!css-loader!sass-loader?sourceMap';
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var ASSETS_PATH_FULLY_QUALIFIED = '/assets/';
+var buildDate = (new Date());
+
+module.exports = {
+  entry: {
+    main: [
+      './app/main.js'
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'build', 'assets'),
+    publicPath: '/assets/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js',
+    sourceMapFilename: 'debug/[file].map',
+    pathinfo: true
+  },
+  cache: false,
+  debug: false,
+  devtool: false,
+  stats: {
+    colors: true,
+    reasons: true
+  },
+
+  resolve: {
+    root: path.join(__dirname, 'app'),
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js', '.jsx']
+  },
+
+  resolveLoader: {
+    root: path.join(__dirname, "node_modules")
+  },
+
+  externals: {},
+
+  module: {
+    // preLoaders: [
+    //   {
+    //     test: /\.js$/,
+    //     exclude: /node_modules/,
+    //     loader: 'eslint-loader'
+    //   }
+    // ],
+    loaders: [{
+      test: /\.hbs$/,
+      loader: "handlebars-loader"
+    }, {
+      test: require.resolve('backbone.validation'),
+      loader: 'expose?Backbone.Validation'
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader?stage=0&optional=runtime'
+    }, {
+      test: /\.scss$/,
+      loader: 'style-loader!css-loader!sass-loader'
+    }, {
+      test: /\.(png|jpg|jpeg|gif|svg)$/,
+      loader: 'url-loader?limit=10000'
+    }, {
+      test: /\.(woff|woff2)$/,
+      loader: 'url-loader?limit=10000'
+    }, {
+      test: /\.(ttf|eot)$/,
+      loader: 'file-loader'
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
+    }, {
+      test: /\.json5$/,
+      loader: 'json5-loader'
+    }, {
+      test: /\.(wav|mp3)$/,
+      loader: 'file-loader'
+    }, {
+      test: /\.html$/,
+      loader: 'html-loader'
+    }, {
+      test: /\.txt$/,
+      loader: 'raw-loader'
+    }, {
+      test: /\.(md|markdown)$/,
+      loaders: ['html-loader', 'markdown-loader']
+    }]
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      },
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Production - ',
+      description: 'Description',
+      filename: '../index.html',
+      minify: {
+        collapseWhitespace: true
+      },
+      inject: false,
+      template: './templates/prod/index.html',
+      buildDate: {
+        unix: buildDate.getTime(),
+        string: buildDate.toString(),
+        date: buildDate.toDateString()
+      }
+    }),
+  ],
+
+  eslint: {
+    configFile: './.eslintrc'
+  }
+};
