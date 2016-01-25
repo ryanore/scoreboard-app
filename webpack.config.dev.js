f8bdfe7937941474c07e6d64c2df96437635fbfb
Ryan Ore <ryan@ryanore.com>
2016-01-25 15:31:43 -0600

    Update environment var

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -40,7 +40,9 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
+      "process.env": {
+        NODE_ENV: JSON.stringify('development')
+      },
       DEBUG: true,
       BROWSER: true
     })
